977fdc1137217993b3f236013d3353fa1a010918
Дмитрий Иванов <dmianiv@mail.ru>
2022-12-30 16:57:57 +0400

    Add arrows for gamefield and figures.

--- maincpp.cpp
+++ maincpp.cpp
@@ -1,11 +1,39 @@
 #include <SFML/Graphics.hpp>
 using namespace sf;
+
+//make a game field
+int const N = 20;
+int const M = 30;
+int gameField[N][M] = { 0 };
+
+//make an arrow for tetramino
+int Figures[7][4] =
+{
+    2,3,4,5,       //O
+    3,4,5,7,       //T
+    3,4,5,6,       //Z
+    3,5,7,6,       //J
+    2,4,6,7,       //L
+    2,4,5,7,       //S
+    0,2,4,6        //I
+};
+ 
+class Point
+{
+public:
+    int x, y;
+};
+Point a[10], b[10];
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(500, 600), "Tetris_Try");
-   // sf::CircleShape shape(150.f);
-   // shape.setFillColor(sf::Color::Green);
+  
+    Texture texture;
+    texture.loadFromFile("C:\\Users\\ƒмитрий\\Desktop\\C++(+MVS)_обучение\\Tetris\\SpriteTry.bmp");
 
+    Sprite sprite(texture);
+    sprite.setTextureRect(IntRect(0, 0, 18, 18));
     while (window.isOpen())
     {
         Event event;
@@ -17,12 +45,7 @@ int main()
 
         window.clear(Color::White);
 
-        Texture texture;
-        texture.loadFromFile("C:\\Users\\ƒмитрий\\Desktop\\C++(+MVS)_обучение\\Tetris\\SpriteTry.bmp");
-        
         texture.setRepeated(true);
-
-        Sprite sprite(texture,IntRect(0,0,400,198));
         window.draw(sprite);
         window.display();
     }
